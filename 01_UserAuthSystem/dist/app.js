5a0d03be46e58d79448735a7df600d2e20344991
SilverFlin <toledorusso@outlook.com>
2023-02-23 19:15:20 -0700

    feat: helmet.js n google oauth endpoints

--- 01_UserAuthSystem/dist/app.js
+++ 01_UserAuthSystem/dist/app.js
@@ -6,19 +6,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const https_1 = __importDefault(require("https"));
+const helmet_1 = __importDefault(require("helmet"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
+/*Securing HTTP through Headers*/
+app.use((0, helmet_1.default)());
+/* */
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true; // TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: 'You must log in'
+        });
+    }
+    next();
+}
+app.get("/auth/google", (req, res) => { });
+app.get("/auth/google/callback", (req, res) => { });
+app.get("/auth/logout", (req, res) => { });
 app.get("/", ((req, res) => {
     res.sendFile(path_1.default.join(__dirname, "../public", "index.html"));
 }));
-app.get("/secret", ((req, res) => {
+app.get("/secret", checkLoggedIn, ((req, res) => {
     res.json({ message: "Secret" });
 }));
 // key and certificate files are generated using OpenSSL
 https_1.default.createServer({
     key: fs_1.default.readFileSync("key.pem"),
     cert: fs_1.default.readFileSync("cert.pem")
-}).listen(PORT, () => {
+}, app).listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
--- 01_UserAuthSystem/src/app.ts
+++ 01_UserAuthSystem/src/app.ts
@@ -1,16 +1,41 @@
 import fs from 'fs'
 import path from 'path'
 import https from 'https'
-import express from 'express'
+
+import helmet from 'helmet'
+import express, { NextFunction, Request, Response } from 'express'
 
 const app = express()
 const PORT = 3000;
+
+/*Securing HTTP through Headers*/
+app.use(helmet())
+
+/* */
+
+function checkLoggedIn(req: Request, res: Response, next: NextFunction) {
+    const isLoggedIn = true; // TODO
+
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: 'You must log in'
+        })
+    }
+
+    next();
+}
+
+app.get("/auth/google", (req, res) => { })
+
+app.get("/auth/google/callback", (req, res) => { })
+
+app.get("/auth/logout", (req, res) => { })
 
 app.get("/", ((req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"))
 }))
 
-app.get("/secret", ((req, res) => {
+app.get("/secret", checkLoggedIn, ((req, res) => {
     res.json({ message: "Secret" })
 }))
 
@@ -18,6 +43,6 @@ app.get("/secret", ((req, res) => {
 https.createServer({
     key: fs.readFileSync("key.pem"),
     cert: fs.readFileSync("cert.pem")
-}).listen(PORT, () => {
+}, app).listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
