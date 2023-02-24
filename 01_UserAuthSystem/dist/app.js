a72c2ef145ae5dab97e6108a44ef10d7e03eff8e
SilverFlin <toledorusso@outlook.com>
2023-02-23 18:50:29 -0700

    feat: https certs

--- 01_UserAuthSystem/dist/app.js
+++ 01_UserAuthSystem/dist/app.js
@@ -3,15 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const https_1 = __importDefault(require("https"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.get("/", ((req, res) => {
-    res.send("test");
+    res.sendFile(path_1.default.join(__dirname, "../public", "index.html"));
 }));
 app.get("/secret", ((req, res) => {
-    res.send("secret");
+    res.json({ message: "Secret" });
 }));
-app.listen(PORT, () => {
+// key and certificate files are generated using OpenSSL
+https_1.default.createServer({
+    key: fs_1.default.readFileSync("key.pem"),
+    cert: fs_1.default.readFileSync("cert.pem")
+}).listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
--- 01_UserAuthSystem/src/app.ts
+++ 01_UserAuthSystem/src/app.ts
@@ -1,16 +1,23 @@
+import fs from 'fs'
+import path from 'path'
+import https from 'https'
 import express from 'express'
 
 const app = express()
 const PORT = 3000;
 
 app.get("/", ((req, res) => {
-    res.send("test")
+    res.sendFile(path.join(__dirname, "../public", "index.html"))
 }))
 
 app.get("/secret", ((req, res) => {
-    res.send("secret")
+    res.json({ message: "Secret" })
 }))
 
-app.listen(PORT, () => {
+// key and certificate files are generated using OpenSSL
+https.createServer({
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem")
+}).listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
