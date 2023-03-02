9eb6b7bb1d981398a3efe55d9359df6117eb0ea2
SilverFlin <toledorusso@outlook.com>
2023-03-01 18:43:19 -0700

    feat: implement get request in controller

--- 03_BookStore_RESTful/src/controllers/bookController.ts
+++ 03_BookStore_RESTful/src/controllers/bookController.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
+import * as bookService from "../services/bookService"
 
 const getAllBooks = (req: Request, res: Response) => {
-    return res.json({ message: `All books` })
+    // TODO add pagination
+    const allBooks = bookService.getAllBooks()
+    return res.json(allBooks)
 }
 
 const getBookById = (req: Request, res: Response) => {
-    return res.json({ message: `Book #${req.params.id} Info.` })
+    let id = req.params.id;
+    const book = bookService.getBookById(id)
+    if (book) {
+        return res.json(book)
+    } else {
+        return res.status(400).json({ error: "Book not found" })
+    }
 }
 
 const postBook = (req: Request, res: Response) => {
